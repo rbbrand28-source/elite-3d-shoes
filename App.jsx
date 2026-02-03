508f77c8d80f2f2b51479b23f772a776f45b4a45
rbbrand28-source <rbbrand28@gmail.com>
2026-02-03 18:30:03 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShoeCanvas from "./components/ShoeCanvas";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center">
+      <ShoeCanvas />
+    </div>
+  );
+}
+
+export default App;
